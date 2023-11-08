4e91874311f1f405020d1367929db0e75c32c7fd
Gerardo "Jerry" Valdez <gvaldez0290@gmail.com>
2023-11-07 23:57:57 -0500

    Update and rename WalmartTainData.sql to WalmartSalesData.sql

--- WalmartSalesData.sql
+++ WalmartSalesData.sql
@@ -2,6 +2,13 @@
 walmart sales forcast dataset was downloaded from Kaggle https://www.kaggle.com/datasets/aslanahmedov/walmart-sales-forecast?select=train.csv
 This was then imported to SQL Server Management Studio (SSMS) by first creating a Walmart DB then importing the csv.
 '''
---Demonstrates that the table has been created in SQL by showing the first 10 rows 
+--Demonstrates that the tables have been created in SQL by showing the first 10 rows 
 SELECT TOP 10 * 
 FROM Walmart.dbo.WalmartTrain
+
+SELECT TOP 10 *
+FROM Walmart.dbo.WalmartSalesFeatures
+
+
+SELECT TOP 10 * 
+FROM Walmart.dbo.stores
